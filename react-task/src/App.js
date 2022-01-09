6317fe8d181e66a3938d722cca72b9aec2770846
Brenna Pavlinchak <raven.mobile.design@gmail.com>
2022-01-09 03:58:09 -0500

    Added button and header files

--- react-task/src/App.js
+++ react-task/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
 
-function App() {
+
+function App() 
+{
   return (
-    <div className="App">
-      <h1>Hello From React</h1>
+    <div className='container'>
+      <Header  />
     </div>
   );
 }
