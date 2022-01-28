9088f1a50dd3c0d321fdad0eb1245718fdac02b4
Brenna Pavlinchak <raven.mobile.design@gmail.com>
2022-01-27 19:48:16 -0500

    Finishing up with Task.js and adding AddTask.js

--- react-task/src/App.js
+++ react-task/src/App.js
@@ -25,10 +25,21 @@ const App = () =>
     },
   ])
   
+  
+  const deleteTask = (id) => 
+  {
+    setTasks(tasks.filter((task) => task.id !==id))
+  }
+  
+  const toggleReminder = (id) =>
+  {
+    setTasks(task.map((task) => task.id === id ? {...task, reminder: ! task.reminder} : task))
+  }
+  
   return (
     <div className='container'>
       <Header  />
-      <Tasks tasks = {tasks} />
+      {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete = {deleteTask} onToggle = {toggleReminder} /> : 'No Tasks'}
     </div>
   )
 }
