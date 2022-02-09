a615f575e2e41986087073ea61e6acfab473f6d7
Brenna Pavlinchak <raven.mobile.design@gmail.com>
2022-02-08 22:32:03 -0500

    Trying to make navbar work

---
+++ projectpax/src/Components-Navbar/Index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from './NavbarElements';
+  
+const Navbar = () => {
+  return (
+	<>
+	  <Nav>
+		<Bars />
+  
+		<NavMenu>
+		  <NavLink to='/About' activeStyle>
+			About
+		  </NavLink>
+		  <NavLink to='/BusinessPlan' activeStyle>
+			Business Plan
+		  </NavLink>
+		  <NavLink to='/GoldenDoodles' activeStyle>
+			GoldenDoodles
+		  </NavLink>
+		  <NavLink to='/Goldens' activeStyle>
+			Goldens
+		  </NavLink>
+		  <NavLink to='/Poodles' activeStyle>
+			Poodles
+		  </NavLink>
+		  <NavLink to='/Resources' activeStyle>
+			Resources
+		  </NavLink>
+		  <NavLink to='/TheFarm' activeStyle>
+			  The Farm
+		  </NavLink>
+		  {/* Second Nav */}
+		  {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+		</NavMenu>
+		<NavBtn>
+		  <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+		</NavBtn>
+	  </Nav>
+	</>
+  );
+};
+  
+export default Navbar;
