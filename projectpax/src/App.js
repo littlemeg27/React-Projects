12c4981fcf28bc6d80b196a533cac47459727b82
Brenna Pavlinchak <raven.mobile.design@gmail.com>
2022-02-08 21:52:55 -0500

    Adding Pages

--- projectpax/src/App.js
+++ projectpax/src/App.js
@@ -1,16 +1,33 @@
-import Header from './Components/Header'
-import Body from './Components/MainBody'
+import React from 'react';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Goldens from './Pages/Goldens';
+import Poodles from './Pages/Poodles';
+import GoldenDoodles from './Pages/GoldenDoodles';
+import TheFarm from './Pages/TheFarm';
+import BusinessPlan from './Pages/BusinessPlan';
+import Resourses from './Pages/Resourses';
+  
 function App() 
 {
   return (
-    <div className='App'>
-      <h1>Project Pax</h1>
-      
-      
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/About' component={About} />
+        <Route path='/Goldens' component={Goldens} />
+        <Route path='/Poodles' component={Poodles} />
+        <Route path='/GoldenDoodles' component={GoldenDoodles} />
+        <Route path='/TheFarm' component={TheFarm} />
+        <Route path='/BusinessPlan' component={BusinessPlan} />
+        <Route path='/Resourses' component={Resourses} />
+      </Switch>
+    </Router>
   );
 }
-
+  
 export default App;
---
+++ projectpax/src/Pages/Goldens.js
@@ -0,0 +1,18 @@
+import React from 'react';
+  
+const Goldens = () => {
+  return (
+	<div
+	  style={{
+		display: 'flex',
+		justifyContent: 'Right',
+		alignItems: 'Right',
+		height: '100vh'
+	  }}
+	>
+	  <h1>Goldens</h1>
+	</div>
+  );
+};
+  
+export default Goldens;
